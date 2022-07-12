bcf8af090047dd7c99427589d366448fe8bbd419
Piotr Kącki <piotrekacki@gmail.com>
2022-07-12 14:43:54 +0200

    Create Ship folder with ships class, BattleShipApp class, GameBoard class, Position class

---
+++ BattleShip/BattleShip/Ship/Patrol Boat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Ship
+{
+    internal class Patrol_Boat : Ship
+    {
+        public override string Name { get; set; } = "PatrolBoat";
+        public override int Length { get; set; } = 2;
+        public override List<Position> Position { get; set; }
+    }
+}
