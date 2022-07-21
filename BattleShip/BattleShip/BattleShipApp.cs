b99b83b7ad302b43cbbba75b905fa514fb95dbca
piotr <piotrekacki@gmail.com>
2022-07-21 16:58:34 +0200

    Implemented course of the game

--- BattleShip/BattleShip/BattleShipApp.cs
+++ BattleShip/BattleShip/BattleShipApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleShip.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,38 +15,90 @@ namespace BattleShip
 
         public void StartGame()
         {
-            var dateTime = DateTime.Now;
+            var startDate = DateTime.Now;
             _firstPlayer = new Player("Player_1");
             _secondPlayer = new Player("Player_2");
-            var gameResult = true;
-            while(gameResult)
+            var roundNumber = 1;
+            
+            while(true)
             {
-                var randFPIndex = _random.Next(_firstPlayer.GameBoard.GameField.Count);
-                var randShot = _firstPlayer.GameBoard.GameField[randFPIndex];
+                SimulateRound();
+                var gameResult = CheckGameResult(out var winner);
 
-                var randSPIndex = _random.Next(_secondPlayer.GameBoard.GameField.Count);
-                var randSPShot = _secondPlayer.GameBoard.GameField[randSPIndex];
-
-                var fpShootPoint = _firstPlayer.GameBoard.GameField.SingleOrDefault(c => c.X == randShot.X && c.Y == randShot.Y);
-                var spShootPoint = _secondPlayer.GameBoard.GameField.SingleOrDefault(c =>c.X == randSPShot.X && c.Y == randSPShot.Y);
-
-                if (fpShootPoint == null || spShootPoint == null)
+                switch (gameResult)
                 {
-                    gameResult = false;
+                    case GameResult.Tie:
+                        Console.WriteLine($"Game ended in {roundNumber} rounds with result: {GameResult.Tie}");
+                        CalculateGameTime(startDate);
+                        return;
+                    case GameResult.GameOver:
+                        Console.WriteLine($"Game ended in {roundNumber} rounds with result: {winner?.Name} is the winner");
+                        CalculateGameTime(startDate);
+                        return;
+                    case GameResult.Playing:
+                        break;
                 }
-                fpShootPoint.IsMarked = true;
-                spShootPoint.IsMarked = true;
-
-                CheckShipsStatuses();
+                roundNumber++;
             }
         }
-        public void CheckShipsStatuses()
+        private void CalculateGameTime(DateTime startDate)   
+        {
+            var endDate = DateTime.Now;
+            var timeDiff = endDate.Subtract(startDate).TotalSeconds;
+            Console.WriteLine($"Simulation took: {timeDiff} seconds");
+        }
+
+        private void SimulateRound()
+        {
+            var randFPIndex = _random.Next(_firstPlayer.GameBoard.GameField.Count);
+            var randFPShot = _firstPlayer.GameBoard.GameField[randFPIndex];
+
+            var randSPIndex = _random.Next(_secondPlayer.GameBoard.GameField.Count);
+            var randSPShot = _secondPlayer.GameBoard.GameField[randSPIndex];
+
+            var fpShootPoint = _firstPlayer.GameBoard.GameField.SingleOrDefault(c => c.X == randFPShot.X && c.Y == randFPShot.Y);
+            var spShootPoint = _secondPlayer.GameBoard.GameField.SingleOrDefault(c => c.X == randSPShot.X && c.Y == randSPShot.Y);
+
+            if (fpShootPoint == null)
+            {
+                throw new ArgumentNullException(nameof(randFPShot), "Incorrect hit point selected!");
+            }
+            else if (spShootPoint == null)
+            {
+                throw new ArgumentNullException(nameof(randSPShot), "Incorrect hit point selected!");
+            }
+            fpShootPoint.IsMarked = true;
+            spShootPoint.IsMarked = true;
+
+            CheckShipsStatuses();
+        }
+        private void CheckShipsStatuses()
         {
             var fpDestroyedShips = _firstPlayer.GameBoard.Ships.Where(s => !s.IsDestroyed && s.ShipCoordinates.All(p => p.IsMarked)).ToList();
             var spDestroyedShips = _secondPlayer.GameBoard.Ships.Where(s => !s.IsDestroyed && s.ShipCoordinates.All(p => p.IsMarked)).ToList();
             fpDestroyedShips.ForEach(s => s.IsDestroyed = true);
             spDestroyedShips.ForEach(s => s.IsDestroyed = true);
 
+        }
+        private GameResult CheckGameResult(out Player? winner)
+        {
+            winner = null; 
+            var firstPlayerDestroyed = _firstPlayer.GameBoard.Ships.All(s => s.IsDestroyed);
+            var secondPlayerDestroyed = _secondPlayer.GameBoard.Ships.All(s => s.IsDestroyed);
+
+            switch (firstPlayerDestroyed)
+            {
+                case true when secondPlayerDestroyed:
+                    return GameResult.Tie;
+                case true:
+                    winner = _secondPlayer;
+                    return GameResult.GameOver;
+                case false when secondPlayerDestroyed:
+                    winner = _firstPlayer;
+                    return GameResult.GameOver;
+                default:
+                    return GameResult.Playing;
+            }
         }
     }
 }
